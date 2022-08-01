4d4628ac5b8a7e9fd4238a18361c23481f7624be
MYLEJEN <95838839+mylejen@users.noreply.github.com>
2022-08-01 10:41:32 +0800

    Update c.js

--- c.js
+++ c.js
@@ -2,31 +2,46 @@ document.documentElement.style.scrollBehavior = "smooth";
 var separate = window.location.hostname.split('.'); separate.shift();
 var domainName = separate.join('.');
 var utmparams = new URLSearchParams(window.location.search);
+
 let checkef2 = document.querySelector("label[for=extra_field_2]") !== null;
 if (checkef2) { var textef2 = document.querySelector("label[for=extra_field_2]").textContent; }
+
 let checkef3 = document.querySelector("label[for=extra_field_3]") !== null;
 if (checkef3) { var textef3 = document.querySelector("label[for=extra_field_3]").textContent; }
 
 let utm_campaign = utmparams.get("utm_campaign")
 let utm_source = utmparams.get("utm_source")
-let utmSource = utmparams.get(encodeURIComponent("utm_source"));
-let utmMedium = utmparams.get(encodeURIComponent("utm_medium"));
-// let utmCampaign = utmparams.get(encodeURIComponent("utm_campaign"));
 
-let utmCampaign1 = utmparams.get("utm_campaign")
-let utmCampaign = encodeURIComponent(utmCampaign1);
+let localutmSource = utmparams.get("utm_source")
+let utmSource = encodeURIComponent(localutmSource);
 
-let utmContent = utmparams.get(encodeURIComponent("utm_content"));
-let utmTerm = utmparams.get(encodeURIComponent("utm_term"));
-let utmID = utmparams.get(encodeURIComponent("utm_id"));
-let utmRef = utmparams.get(encodeURIComponent("ref"));
-let utmFirstCampaign = utmparams.get(encodeURIComponent("utm_campaign"));
+let localutmMedium = utmparams.get("utm_medium")
+let utmMedium = encodeURIComponent(localutmMedium);
+
+let localutmCampaign = utmparams.get("utm_campaign")
+let utmCampaign = encodeURIComponent(localutmCampaign);
+
+let localutmFirstCampaign = utmparams.get("utm_campaign")
+let utmFirstCampaign = encodeURIComponent(localutmFirstCampaign);
+
+let localutmContent = utmparams.get("utm_content")
+let utmContent = encodeURIComponent(localutmContent);
+
+let localutmTerm = utmparams.get("utm_term")
+let utmTerm = encodeURIComponent(localutmTerm);
+
+let localutmID = utmparams.get("utm_id")
+let utmID = encodeURIComponent(localutmID);
+
+let localutmRef = utmparams.get("ref")
+let utmRef = encodeURIComponent(localutmRef);
 
 if (utmparams.get("utm_campaign")){
 sessionStorage.setItem('utm_campaign', utmparams.get("utm_campaign"))};
 
 if (utmparams.get("utm_source")){
 sessionStorage.setItem('utm_source', utmparams.get("utm_source"))};
+
 utm_campaign = sessionStorage.utm_campaign
 utm_source = sessionStorage.utm_source
 
@@ -49,7 +64,8 @@ let exdays = 360;
 d.setTime(d.getTime() + (exdays*24*60*60*1000));
 let expires = "expires="+ d.toUTCString();
 
-if (utmSource !== null || utmMedium !== null || utmCampaign !== null || utmContent !== null || utmTerm !== null || utmID !== null || utmRef !== null) {
+if (utmSource !== null || utmMedium !== null || utmCampaign !== null || utmContent !== null || utmTerm !== null || utmID !== null) {
+
 document.cookie = "muSource=" + utmSource + ";" + expires + ";path=/; secure; domain=." + domainName;
 document.cookie = "muMedium=" + utmMedium + ";" + expires + ";path=/; secure; domain=." + domainName;
 document.cookie = "muCampaign=" + utmCampaign + ";" + expires + ";path=/; secure; domain=." + domainName;
@@ -57,36 +73,47 @@ document.cookie = "muContent=" + utmContent + ";" + expires + ";path=/; secure; 
 document.cookie = "muTerm=" + utmTerm + ";" + expires + ";path=/; secure; domain=." + domainName;
 document.cookie = "muID=" + utmID + ";" + expires + ";path=/; secure; domain=." + domainName;
 document.cookie = "muRef=" + utmRef + ";" + expires + ";path=/; secure; domain=." + domainName;
-localStorage.setItem('muSource', utmSource);
-localStorage.setItem('muMedium', utmMedium);
-localStorage.setItem('muCampaign', utmCampaign);
-localStorage.setItem('muContent', utmContent);
-localStorage.setItem('muTerm', utmTerm);
-localStorage.setItem('muID', utmID);
-localStorage.setItem('muRef', utmRef); }
+  
+localStorage.setItem('muSource', localutmSource);
+localStorage.setItem('muMedium', localutmMedium);
+localStorage.setItem('muCampaign', localutmCampaign);
+localStorage.setItem('muContent', localutmContent);
+localStorage.setItem('muTerm', localutmTerm);
+localStorage.setItem('muID', localutmID);
+localStorage.setItem('muRef', localutmRef);
+
+}
 
 if (utmFirstCampaign !== null) {
+  
 let cFirstCampaign = ('; '+document.cookie).split(`; muFirstCampaign=`).pop().split(';')[0];
-if (cFirstCampaign == "" || cFirstCampaign == null || cFirstCampaign == "null") {
-localStorage.setItem('muFirstCampaign', utmFirstCampaign);
-document.cookie = "muFirstCampaign=" + utmFirstCampaign + ";" + expires + ";path=/; secure; domain=." + domainName; }}
+
+      if (cFirstCampaign == "" || cFirstCampaign == null || cFirstCampaign == "null") {
+          localStorage.setItem('muFirstCampaign', localutmFirstCampaign);
+          document.cookie = "muFirstCampaign=" + utmFirstCampaign + ";" + expires + ";path=/; secure; domain=." + domainName;
+          }
+          
+}
 
 sessionStorage.setItem("hasRunAxios", "0");
 localStorage.setItem("hasRunAxios", "0");
 
 let checktarikh = document.getElementById("tarikh") !== null;
-if (checktarikh) { const now = new Date();
+if (checktarikh) {
+const now = new Date();
 let days = new Array('Ahad','Isnin','Selasa','Rabu','Khamis','Jumaat','Sabtu');
 let months = new Array('Januari','Februari','Mac','April','Mei','Jun','Julai','Ogos','September','Oktober','November','Disember');
 let date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
 function fourdigits(number) {
 return (number < 1000) ? number + 1900 : number; }
 today = days[now.getDay()] + ", " + date + " " + months[now.getMonth()] + " " + (fourdigits(now.getYear())) ;
-document.getElementById("tarikh").innerHTML = today; }
+document.getElementById("tarikh").innerHTML = today;
+}
 
 let lejen = document.querySelector("script[src*=utm\\@latest]")
 let sasaran = lejen.getAttribute("sasaran")
 let checklejen = sasaran !== null;
+
 if (checklejen) {
 var customDonationCurrentValue = Number(PageData.salesStats.total_amount_all);
 var customDonationTargetValue = parseFloat(sasaran);
