ee0376f24e37be692d29a9ca3d3608f9367ac64b
MYLEJEN <95838839+mylejen@users.noreply.github.com>
2022-08-01 22:56:46 +0800

    Update db1.js

--- db1.js
+++ db1.js
@@ -2,14 +2,15 @@ var pabblyid = "IjQ2NzU2MCI_3D"
 
 if (lejenuser == "MSP") {
 
-offlineeventid = "1881191202066799"
-metapixelid = "450239113467284"
-adaccountid = "873510686873021"
-audienceid = "23850985273040614"
+var offlineeventid = "1881191202066799"
+var metapixelid = "450239113467284"
+var adaccountid = "873510686873021"
+var audienceid = "23850985273040614"
 
 console.log("oid 1 "+ offlineeventid)
 console.log("pid 1 "+ metapixelid)
 console.log("act 1 "+ adaccountid)
 console.log("aid 1 "+ audienceid)
+console.log("pabblyid 1 "+ pabblyid)
 
 }
