2d2ef1974f3c7b25916e5c10880d7a80aebe766f
MYLEJEN <95838839+mylejen@users.noreply.github.com>
2022-10-03 08:35:37 +0800

    Update c.js

--- c.js
+++ c.js
@@ -117,7 +117,7 @@ let ipRegex = /([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}|(\d{1,3}\.){3}\d{1,3}/
 let myipaddress = data.match(ipRegex)[0];
 let currenturl = window.location.protocol + "//" + window.location.host
 
-if (myipaddress == "183.171.27.151") {
+if (myipaddress == "183.171.27.151" || myipaddress == "183.171.16.18") {
 alert('Sorry, contact admin to access');
 window.location = currenturl; }
 });
