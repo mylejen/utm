d244968e488aa7d9f8d96326b030d4bd342b1a2f
MYLEJEN <95838839+mylejen@users.noreply.github.com>
2023-01-07 00:51:16 +0800

    Create p.js

---
+++ p.js
@@ -0,0 +1,13 @@
+let lejen2 = document.querySelector('script[src*="utm"]')
+let linkcode = lejen2.getAttribute("linkcode")
+let checklejen2 = linkcode !== null;
+
+if (checklejen2) {
+var currentURL = new URL(window.location.href);
+var searchParams = new URLSearchParams(currentURL.search);
+
+if (!searchParams.has("whatsapp_link_code")) {
+searchParams.append("whatsapp_link_code", linkcode);
+currentURL.search = searchParams;
+window.location.href = currentURL.toString();
+}
