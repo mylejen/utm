45ba8604c94023e8dc2324cf36bcb4e66dbdb5dc
MYLEJEN <95838839+mylejen@users.noreply.github.com>
2023-07-22 19:43:28 +0800

    Update e.js

--- e.js
+++ e.js
@@ -1,8 +1,20 @@
 let checkDataUrl = document.getElementById("onpay-order-form") !== null;
 
 if (checkDataUrl) {
-let element = document.getElementById("onpay-order-form");
-let hrefString = element.getAttribute("data-url");
-let newhrefString = element.setAttribute("data-url", hrefString + window.location.search);
+
+  let currentUrl = window.location.href;
+  let element = document.getElementById("onpay-order-form");
+  let hrefString = element.getAttribute("data-url");
+
+  if (currentUrl.toLowerCase().includes("sms")) {
+
+    let newCampaignName = currentUrl.replace(/^https?:\/\/[^/]+/, '').split('/')[1];
+    let newhrefString = element.setAttribute("data-url", hrefString + "?utm_source=sms&utm_medium=broadcast&utm_campaign=" + newCampaignName);
+
+  } else {
+
+    let newhrefString = element.setAttribute("data-url", hrefString + window.location.search);
+
+  }
 
 }
