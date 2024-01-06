e7fbc14a9fd51a30a1bbda11007cf02ad94ead0e
MYLEJEN <95838839+mylejen@users.noreply.github.com>
2024-01-06 15:21:05 +0800

    Update e.js

--- e.js
+++ e.js
@@ -17,4 +17,42 @@ if (checkDataUrl) {
 
   }
 
+// Cookies from iframe domain
+  
+  function getDomainFromUrl(url) {
+    var parsedUrl = new URL(url);
+    return parsedUrl.hostname;
+  }
+
+  var iframeDomain = getDomainFromUrl(hrefString);
+
+    window.addEventListener("message", receiveMessage, false);
+
+    function receiveMessage(event) {
+        var allowedOrigin = "https://" + iframeDomain;  // Replace with your iframe domain
+
+        if (event.origin !== allowedOrigin) {
+            console.log("Origin not allowed");
+            return;
+        }
+
+        if (event.data === "requestCookies") {
+            var cookies = {
+                _ttp: getCookie("_ttp"),
+                _fbc: getCookie("_fbc"),
+                _fbp: getCookie("_fbp")
+            };
+            event.source.postMessage({ cookies: cookies }, event.origin);
+        }
+    }
+
+    function getCookie(name) {
+        var value = "; " + document.cookie;
+        var parts = value.split("; " + name + "=");
+        if (parts.length === 2) return parts.pop().split(";").shift();
+        return null;  // Returns null if the cookie doesn't exist
+    }
+
 }
+
+
